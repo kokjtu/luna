46156eafbefca756a8be65a7ecdbccbd3b1831bb
cuong pham <cuong.pt0622@outlook.com.vn>
2021-09-20 19:42:25 +0700

    test

--- app/routes/room.js
+++ app/routes/room.js
@@ -10,7 +10,9 @@ require('../middlewares/passport')
 
 router.use(passport.authenticate('jwt', { session: false }))
 
-router.route('/').get(RoomController.getRooms)
+router.route('/').get((req, res, next) => {
+  res.status(200).json({'rooms': true})
+})
 
 router
   .route('/:roomID')
